50c62e6a7b1bee0495ea9a2180434fdd75d0aa70
Shobhit Arya <shobhitarya2002@gmail.com>
2024-02-05 01:35:16 +0530

    New Build

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
 
 const http = require('http');
 const {Server} = require('socket.io');
 
 const server = http.createServer(app);
 const io = new Server(server);
-app.use(cors());
 
 const ACTIONS = require( './src/utils/Actions');
 
